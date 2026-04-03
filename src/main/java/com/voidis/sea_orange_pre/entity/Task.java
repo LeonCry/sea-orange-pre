757aa73fb7d2d15f9e4dc88d80184c16b61efa64
voidis <lbh_ldu@outlook.com>
2026-04-03 15:51:07 +0800

    init

---
+++ src/main/java/com/voidis/sea_orange_pre/entity/Task.java
@@ -0,0 +1,20 @@
+package com.voidis.sea_orange_pre.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+}
