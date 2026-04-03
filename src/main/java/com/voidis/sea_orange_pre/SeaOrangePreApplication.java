789d2094d666bf283396bae345f020fe2f6ade09
voidis <lbh_ldu@outlook.com>
2026-04-03 13:54:39 +0800

    init

---
+++ src/main/java/com/voidis/sea_orange_pre/SeaOrangePreApplication.java
@@ -0,0 +1,13 @@
+package com.voidis.sea_orange_pre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaOrangePreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaOrangePreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voidis/sea_orange_pre/SeaOrangePreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voidis.sea_orange_pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaOrangePreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
